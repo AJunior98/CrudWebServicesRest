6de8172ccb763a6cf2f34836c2414f8bb34438fd
Amauri Junior <apojr98@gmail.com>
2022-06-13 15:52:38 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('João', '77788899945', 1.182, '1979-04-19',2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Maria', '66644423545', 2.323, '2007-06-26',3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Pedro', '4589632145', 3.434, '2003-02-25',0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Junior', '47151523807', 4.564, '1998-01-31',1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Alan', '96587412358', 5.663, '1989-09-29',0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Enzo', '35684213598', 6.789, '1995-10-14',2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Isabela', '32546987452', 7.867, '1993-12-11',3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Cavani', '23546897452', 8.910, '2001-11-09',4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Ronaldo', '23564895423', 4.199, '1991-08-08',3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Messi', '74589653245', 2.256, '1983-05-01',2);
