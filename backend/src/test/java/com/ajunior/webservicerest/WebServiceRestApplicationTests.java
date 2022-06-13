8fdb96b469412b533b5ef7e7825540c390252ba5
Amauri Junior <apojr98@gmail.com>
2022-06-13 09:22:27 -0300

    Backend Project created

---
+++ backend/src/test/java/com/ajunior/webservicerest/WebServiceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajunior.webservicerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
